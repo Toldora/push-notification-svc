c0851770bc13b84f090be131424b2ce78f757087
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2024-07-10 14:37:37 +0300

    enabled cors

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ const PORT = process.env.PORT || 3030;
 
 const app = express();
 
-app.use(
-  cors({
-    origin: false,
-  }),
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/subscriptions', subscriptionsRoutes);
